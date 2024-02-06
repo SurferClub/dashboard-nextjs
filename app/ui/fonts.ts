814fa6b188cb084b8cc10c374dc783273090556f
SurferClub <harley98.01@hotmail.com>
2024-02-06 08:05:18 -0500

    hello dashboard con next

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google';
+import { Roboto_Mono } from 'next/font/google'
+
+export const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+});
+export const lusi = Roboto_Mono({
+    subsets: ['latin'],
+    display: 'swap',
+});
